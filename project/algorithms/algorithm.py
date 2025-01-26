91acdbb0d4ea07edc6e78d7600ffda94036adaf8
Robin Uhrich <robin.uhrich@gmail.com>
2025-01-26 13:07:21 +0100

    added save_hyperparameters method

--- project/algorithms/algorithm.py
+++ project/algorithms/algorithm.py
@@ -1,4 +1,6 @@
 from abc import ABC, abstractmethod
+from argparse import Namespace
+import inspect
 from pathlib import Path
 from typing import List
 
@@ -29,6 +31,30 @@ class RLAlgorithm(ABC):
         self._log_dir = (
             log_dir if log_dir is not None else Path().cwd().joinpath("results/")
         )
+        self._log_dir = Path(self._log_dir) if not isinstance(self._log_dir, Path) else self._log_dir
+
+        self.hparams = Namespace()
+
+    def save_hyperparmeters(self, *args):
+        # Get the frame of the calling function (i.e., the __init__ method)
+        frame = inspect.currentframe().f_back
+        
+        # Get the arguments of the calling function
+        arg_values = frame.f_locals
+        
+        # pre filter
+        exclude_args = ["self", "env", "logger", "eval_env", "kwargs", "args", "__class__"]
+        hparams = {k: v for k, v in arg_values.items() if k not in exclude_args}
+        
+        if args:
+            # Save only specified arguments
+            hparams = {arg: hparams[arg] for arg in args if arg in hparams}
+        else:
+            # Save all arguments except 'self'
+            pass
+
+        # Store in self.hparams
+        self.hparams = Namespace(**hparams)
 
     def update_env(self, env: Env):
         self._env = env
@@ -45,8 +71,8 @@ class RLAlgorithm(ABC):
     def save_checkpoint(self, episode_idx: int, path: Path | str = None):
         raise NotImplementedError
 
-    @abstractmethod
     @classmethod
+    @abstractmethod
     def from_checkpoint(
         cls, checkpoint: str | Path, config: SACConfig
     ) -> "RLAlgorithm":
