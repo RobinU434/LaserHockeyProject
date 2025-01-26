7377503d9860be0decf0ee8347cd7d22de14b5e0
Robin Uhrich <robin.uhrich@gmail.com>
2025-01-26 16:16:37 +0100

    updated config location for c2c

--- c2c.sh
+++ c2c.sh
@@ -2,4 +2,5 @@
 
 config2class stop-all
 
-config2class to-code --input config/train_sac.yaml --output project/utils/configs/train_sac_config.py --init-none
+config2class to-code --input config/training/train_sac.yaml --output project/utils/configs/train_sac_config.py --init-none
+
