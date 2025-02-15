95b216ccf4034fb112ba154871f42e8bfd5a1f40
Robin Uhrich <robin.uhrich@gmail.com>
2025-02-15 11:55:07 +0100

    implemented MultiDiscrete Qnet

--- project/algorithms/dyna/dyna.py
+++ project/algorithms/dyna/dyna.py
@@ -1,22 +1,28 @@
 import math
 from typing import Tuple
+
 import numpy as np
 import pandas as pd
 import torch
+from gymnasium.spaces import Discrete, MultiDiscrete
+from torch import Tensor
+from torch.nn import functional as F
 from tqdm import tqdm
+
 from project.algorithms.common.agent import _Agent
 from project.algorithms.common.algorithm import _RLAlgorithm
 from project.algorithms.common.buffer import ReplayBuffer
-from project.algorithms.common.q_net import DiscreteQNet
-from project.algorithms.dyna.env_model import EnvModel
-from project.algorithms.dyna.q_net import QNet
-from project.algorithms.utils import PlaceHolderEnv, get_space_dim
-from gymnasium.spaces import Discrete
-from torch import Tensor
+from project.algorithms.common.q_net import _QNet
+from project.algorithms.dyna.dyna import _DynaQ
+from project.algorithms.dyna.q_net import MultiDiscreteQNet, QNet
+from project.algorithms.dyna.world_model import WorldModel
+from project.algorithms.utils.encoding import multi_hot
+from project.algorithms.utils.gym_helper import PlaceHolderEnv, get_space_dim
 
-class DynaQ(_RLAlgorithm):
+
+class _DynaQ(_RLAlgorithm):
     def __init__(
-        self,
+self,       
         env,
         logger=[],
         eval_env=None,
@@ -27,7 +33,7 @@ class DynaQ(_RLAlgorithm):
         buffer_limit: int = 50_000,
         start_buffer_size: int = 1000,
         epsilon_decay: float = 0.99,  # decay epsilon greedy policy
-        gamma: float = 0.99,  # discount factor 
+        gamma: float = 0.99,  # discount factor
         tau: float = 0.01,  # soft update parameter
         simulation_updates: int = 2,
         *args,
@@ -43,31 +49,25 @@ class DynaQ(_RLAlgorithm):
             *args,
             **kwargs,
         )
-        assert isinstance(
-            self._env.action_space, Discrete
-        ), "Dyna requires a discrete action space"
 
         self._batch_size = batch_size
         self._buffer_limit = buffer_limit
         self._start_buffer_size = start_buffer_size
         self._epsilon_decay = epsilon_decay  # decay epsilon greedy policy
-        self._gamma = gamma  # discount factor 
+        self._gamma = gamma  # discount factor
         self._tau = tau  # soft update factor
         self._simulation_updates = simulation_updates
         self.save_hyperparmeters()
 
         self._state_dim = get_space_dim(self._env.observation_space)  # noqa: F821
-        self._n_actions = self._env.action_space.n
 
-        self.q_net = QNet(self._state_dim, self._n_actions)
-        self.q_target = QNet(self._state_dim, self._n_actions)
-        self.q_target.load_state_dict(self.q_net.state_dict().copy())
-
-        # note n_actions = action dim because of one hot encoding -> easiest encoding but others are possible
-        self.world_model = EnvModel(self._state_dim, self._n_actions)
         self.memory = ReplayBuffer(self._buffer_limit)
 
         self.total_steps: int
+
+        self.q_net: _QNet
+        self.q_target: _QNet
+        self.world_model: WorldModel
 
     def _get_epsilon(self, total_steps: int):
         return math.exp(-self._epsilon_decay * total_steps)
@@ -81,19 +81,7 @@ class DynaQ(_RLAlgorithm):
         Returns:
             Tensor: action encoding of with a specific action dimension
         """
-        # NOTE: only one hot
-        if isinstance(action, int):
-            a = torch.zeros(self._n_actions)
-            a[action] = 1
-            a = a.float()
-            return a
-        
-        action = action.int()
-        batch_size = len(action)
-        a = torch.zeros(batch_size, self._n_actions, dtype=float)
-        a[torch.arange(batch_size, dtype=int), action] = 1
-        a = a.float()
-        return a
+        raise NotImplementedError
 
     def get_action(self, state: np.ndarray) -> Tuple[int, float]:
         """epsilon greedy policy
@@ -104,17 +92,18 @@ class DynaQ(_RLAlgorithm):
         Returns:
             int: discrete action
         """
-        # decay epsilon exponentially
-        epsilon = self._get_epsilon(self.total_steps)
-        if np.random.uniform() > epsilon:  # greedy
-            action_log_probs = self.q_net.action_log_probs(state)
-            action = torch.argmax(action_log_probs).item()
-            log_prob = action_log_probs[action]
-            return action, log_prob
-        
-        action = np.random.randint(0, self._n_actions)
-        log_prob = -math.log(self._n_actions)
-        return action, log_prob
+        raise NotImplementedError
+
+    def get_max_q(self, state: Tensor) -> Tensor:
+        """get maximum q value of target network
+
+        Args:
+            state (Tensor): (batch_size, state_dim)
+
+        Returns:
+            Tensor: (batch_size, 1)
+        """
+        raise NotImplementedError
 
     def simulation_training(self, episode_idx: int):
         metrics = []
@@ -123,22 +112,22 @@ class DynaQ(_RLAlgorithm):
 
             action_enc = self.encode_action(action)
             with torch.no_grad():
-                next_state_pred, reward_pred, done_pred = self.world_model.forward(state, action_enc)
+                next_state_pred, reward_pred, done_pred = self.world_model.forward(
+                    state, action_enc
+                )
             minibatch = state, action, next_state_pred, reward_pred, done_pred
             target = self.calculate_target(minibatch)
-            self.q_net.train(minibatch, target)
+            self.q_net.train_net(minibatch, target)
 
             info = self.q_net.soft_update(self.q_target, self._tau)
             metrics.append(info)
-        
+
         metrics = pd.DataFrame(metrics).mean().to_dict()
         self.log_dict(metrics, episode_idx, prefix=self.get_name() + "/sim_")
 
     def update_world_model(
         self,
-        mini_batch: Tuple[
-            Tensor, Tensor, Tensor, Tensor, Tensor
-        ],
+        mini_batch: Tuple[Tensor, Tensor, Tensor, Tensor, Tensor],
         episode_idx: int,
     ):
         state, action, next_state, reward, done = mini_batch
@@ -148,34 +137,40 @@ class DynaQ(_RLAlgorithm):
 
     def calculate_target(
         self,
-        mini_batch: Tuple[
-            Tensor, Tensor, Tensor, Tensor, Tensor
-        ],
-    ):
+        mini_batch: Tuple[Tensor, Tensor, Tensor, Tensor, Tensor],
+    ) -> Tensor:
+        """calculate TD target for q values
+
+        Args:
+            mini_batch (Tuple[Tensor, Tensor, Tensor, Tensor, Tensor]): content:
+                state: (batch_size, state_dim)
+                action: (batch_size, action_dim) in the discrete case action_dim=1
+                next_state: (batch_size, state_dim)
+                reward: (batch_size, 1)
+                done: (batch_size, 1)
+
+        Returns:
+            Tensor: td target. (batch_size, 1)
+        """
         _, _, next_state, reward, done = mini_batch
         with torch.no_grad():
-            q_next = self.q_target.forward(next_state)
-            q_next_max = torch.max(q_next, dim=-1, keepdim=True)[0]
+            q_next_max = self.get_max_q(next_state)
 
-        # print(reward, self._gamma, q_next_max, done)
         q_target = reward + self._gamma * q_next_max * (1 - done)
         return q_target
 
     def update_q(
         self,
-        mini_batch: Tuple[
-            Tensor, Tensor, Tensor, Tensor, Tensor
-        ],
+        mini_batch: Tuple[Tensor, Tensor, Tensor, Tensor, Tensor],
         episode_idx: int,
     ):
         target = self.calculate_target(mini_batch)
-        info = self.q_net.train(mini_batch, target)
+        info = self.q_net.train_net(mini_batch, target)
         self.log_dict(info, episode_idx, prefix=self.get_name() + "/")
 
         # make cautious update of target network
         self.q_net.soft_update(self.q_target, self._tau)
 
-        
     def train(self, n_episodes, verbose=False):
         if isinstance(self._env, PlaceHolderEnv):
             raise ValueError(
@@ -197,11 +192,14 @@ class DynaQ(_RLAlgorithm):
             state, _ = self._env.reset()
             while not (done or truncated):
                 action, log_prob = self.get_action(torch.from_numpy(state))
+                # Discrete vs MultiDiscrete
                 next_state, reward, done, truncated, _ = self._env.step(action)
+                if isinstance(action, int):
+                    action = np.array([action], dtype=int)
 
                 self.memory.put(
                     observation=torch.from_numpy(state),
-                    action=torch.tensor([action], dtype=int),
+                    action=torch.from_numpy(action),
                     next_observation=torch.from_numpy(next_state),
                     reward=torch.tensor([reward]),
                     done=torch.tensor([done], dtype=float),
@@ -213,19 +211,104 @@ class DynaQ(_RLAlgorithm):
                     self.update_world_model(mini_batch, episode_idx)
                     self.update_q(mini_batch, episode_idx)
                     self.simulation_training(episode_idx)
-                
+
                 score += reward
                 episode_steps += 1
                 log_probs += log_prob
-                
+
                 state = next_state
                 self.total_steps += 1
 
             self.log_scalar("mean_score", score / episode_steps, episode_idx)
             self.log_scalar("episode_steps", episode_steps, episode_idx)
-            self.log_scalar(self.get_name() + "/log_prob", log_probs / episode_steps, episode_idx)
+            self.log_scalar(
+                self.get_name() + "/log_prob", log_probs / episode_steps, episode_idx
+            )
 
 
+class DynaQ(_DynaQ):
+    def __init__(
+        self,
+        env,
+        logger=[],
+        eval_env=None,
+        eval_check_interval=None,
+        save_interval=None,
+        log_dir=None,
+        batch_size: int = 256,
+        buffer_limit: int = 50_000,
+        start_buffer_size: int = 1000,
+        epsilon_decay: float = 0.99,  # decay epsilon greedy policy
+        gamma: float = 0.99,  # discount factor
+        tau: float = 0.01,  # soft update parameter
+        simulation_updates: int = 2,
+        *args,
+        **kwargs,
+    ):
+        super().__init__(
+            env,
+            logger,
+            eval_env,
+            eval_check_interval,
+            save_interval,
+            log_dir,
+            batch_size,
+            buffer_limit,
+            start_buffer_size,
+            epsilon_decay,
+            gamma,
+            tau,
+            simulation_updates,
+            *args,
+            **kwargs,
+        )
+        # TODO: test is not good. only Discrete but this conflicts MultiDiscrete descendent
+        assert isinstance(
+            self._env.action_space, Discrete
+        ), "Dyna requires a discrete action space"
+
+        self._n_actions = self._env.action_space.n
+
+        self.q_net: QNet = QNet(self._state_dim, self._n_actions)
+        self.q_target: QNet = QNet(self._state_dim, self._n_actions)
+        self.q_target.load_state_dict(self.q_net.state_dict().copy())
+
+        # note n_actions = action dim because of one hot encoding -> easiest encoding but others are possible
+        self.world_model = WorldModel(self._state_dim, self._n_actions)
+
+    def get_max_q(self, state):
+        q_value = self.q_target.complete_forward(state)
+        q_max = torch.max(q_value, dim=-1, keepdim=True)[0]
+        return q_max
+
+    def encode_action(self, action: int | Tensor) -> Tensor:
+        # NOTE: only one hot
+        if isinstance(action, int):
+            a = torch.zeros(self._n_actions)
+            a[action] = 1
+            a = a.float()
+            return a
+
+        action = action.int()
+        batch_size = len(action)
+        a = torch.zeros(batch_size, self._n_actions, dtype=float)
+        a[torch.arange(batch_size, dtype=int), action] = 1
+        a = a.float()
+        return a
+
+    def get_action(self, state: np.ndarray) -> Tuple[int, float]:
+        # decay epsilon exponentially
+        epsilon = self._get_epsilon(self.total_steps)
+        if np.random.uniform() <= epsilon:  # random action
+            action = np.random.randint(0, self._n_actions)
+            log_prob = -math.log(self._n_actions)
+            return action, log_prob
+
+        # greedy action
+        action_log_probs = self.q_net.action_log_probs(state)
+        action = torch.argmax(action_log_probs).item()
+        log_prob = action_log_probs[action]
+        return action, log_prob
 
     def save_checkpoint(self, episode_idx, path=None):
         return super().save_checkpoint(episode_idx, path)
@@ -236,13 +319,136 @@ class DynaQ(_RLAlgorithm):
     def get_agent(self, deterministic=True):
         return super().get_agent(deterministic)
 
-class DynaQQ(DynaQ):
+
+class DynaQQ(_DynaQ):
     pass
 
-class MultiDiscreteDynaQ(DynaQ):
-    pass
 
-class MultiDiscreteDynaQQ(DynaQ):
+class MultiDiscreteDynaQ(_DynaQ):
+    def __init__(
+        self,
+        env,
+        logger=[],
+        eval_env=None,
+        eval_check_interval=None,
+        save_interval=None,
+        log_dir=None,
+        batch_size=256,
+        buffer_limit=50000,
+        start_buffer_size=1000,
+        epsilon_decay=0.99,
+        gamma=0.99,
+        tau=0.01,
+        simulation_updates=2,
+        mc_sample=10,  # switch it of by setting it None. Then do whole sweep
+        *args,
+        **kwargs,
+    ):
+        super().__init__(
+            env,
+            logger,
+            eval_env,
+            eval_check_interval,
+            save_interval,
+            log_dir,
+            batch_size,
+            buffer_limit,
+            start_buffer_size,
+            epsilon_decay,
+            gamma,
+            tau,
+            simulation_updates,
+            *args,
+            **kwargs,
+        )
+        assert isinstance(
+            self._env.action_space, MultiDiscrete
+        ), "expected MultiDiscrete action space"
+
+        self._batch_size = batch_size
+        self._buffer_limit = buffer_limit
+        self._start_buffer_size = start_buffer_size
+        self._epsilon_decay = epsilon_decay  # decay epsilon greedy policy
+        self._gamma = gamma  # discount factor
+        self._tau = tau  # soft update factor
+        self._simulation_updates = simulation_updates
+        self.save_hyperparmeters()
+
+        self._mc_sample = mc_sample
+        self.save_hyperparmeters()
+
+        self._state_dim = get_space_dim(self._env.observation_space)  # noqa: F821
+        action_space: MultiDiscrete = self._env.action_space
+        self._action_dim = action_space.nvec.sum()
+        self._n_actions = action_space.nvec.prod()
+
+        self.q_net: MultiDiscreteQNet = MultiDiscreteQNet(
+            self._state_dim, action_space.nvec
+        )
+        self.q_target: MultiDiscreteQNet = MultiDiscreteQNet(
+            self._state_dim, action_space.nvec
+        )
+        self.q_target.load_state_dict(self.q_net.state_dict().copy())
+
+        self.world_model = WorldModel(self._state_dim, self._action_dim)
+
+    def encode_action(self, action: Tensor) -> Tensor:
+        """converts the discrete action into an action encoding
+
+        Args:
+            action (int | Tensor): discrete action in index space (batch_size, feature_dim).
+
+        Returns:
+            Tensor: action encoding of with a specific action dimension (batch_size, encoding_dim)
+        """
+
+        if len(action.shape) == 1:
+            action = action[None]
+
+        nvec = torch.from_numpy(self._env.action_space.nvec).int()
+        action = multi_hot(action.int(), nvec).float()
+        return action
+
+    def get_max_q(self, state):
+        if self._mc_sample is None:
+            q_value, _ = self.q_target.complete_forward(state)
+        else:
+            q_value, _ = self.q_target.mc_forward(state, self._mc_sample)
+        q_value = q_value[..., 0]
+        q_max = torch.max(q_value, dim=-1, keepdim=True)[0]
+        return q_max
+
+    def get_action(self, state):
+        epsilon = self._get_epsilon(self.total_steps)
+        if np.random.uniform() <= epsilon:  # random action
+            action_idx = np.random.randint(0, self._n_actions)
+            action = self.q_net._all_actions[action_idx]
+            log_prob = -math.log(self._n_actions)
+            return action, log_prob
+
+        # greedy action
+        if self._mc_sample is None:
+            q_val, actions = self.q_net.complete_forward(state)
+        else:
+            q_val, actions = self.q_net.mc_forward(state, self._mc_sample)
+        q_val = q_val[0]
+        q_val = F.log_softmax(q_val, dim=0)
+        idx = q_val.argmax(dim=0)
+        log_prob = q_val[idx]
+        action = actions[idx]
+        return action, log_prob
+
+    def save_checkpoint(self, episode_idx, path=None):
+        return super().save_checkpoint(episode_idx, path)
+
+    def load_checkpoint(self, checkpoint):
+        return super().load_checkpoint(checkpoint)
+
+    def get_agent(self, deterministic=True):
+        return super().get_agent(deterministic)
+
+
+class MultiDiscreteDynaQQ(_DynaQ):
     pass
 
 
--- project/algorithms/dyna/q_net.py
+++ project/algorithms/dyna/q_net.py
@@ -1,5 +1,6 @@
 from typing import Dict, Tuple
 from project.algorithms.common.q_net import DiscreteQNet
+from project.algorithms.common.q_net import MultiDiscreteQNet as _MultiDiscreteQNet
 import torch
 from torch import Tensor
 from torch import nn
@@ -23,26 +24,69 @@ class QNet(DiscreteQNet):
         self.optimizer = torch.optim.AdamW(self.parameters(), lr=lr)
         self.criterion = torch.nn.MSELoss()
 
-    def train(
+    def train_net(
         self, mini_batch: Tuple[Tensor, Tensor, Tensor, Tensor, Tensor], target: Tensor
     ) -> Dict[str, float]:
         state, action, _, _, _ = mini_batch
-        
+
         action = action.int()
         if action.shape[1] == 1:
             action = action[:, 0]
         
-        prediction = self.forward(state)
-        prediction = prediction[torch.arange(len(action)), action]
-        
+        prediction = self.forward(state, action)
+        # prediction = prediction[torch.arange(len(action)), action]
         loss = self.criterion.forward(prediction, target[:, 0])
         self.optimizer.zero_grad()
         loss.backward()
         self.optimizer.step()
-        
-        return {"critic_loss": loss.item()}
-    
-    def soft_update(self, net_target: nn.Module, tau: float):
-        for param_target, param in zip(net_target.parameters(), self.parameters()):
-            param_target.data.copy_(param_target.data * (1.0 - tau) + param.data * tau)
 
+        return {"critic_loss": loss.item()}
+
+
+class MultiDiscreteQNet(_MultiDiscreteQNet):
+    def __init__(
+        self,
+        state_dim,
+        nvec,
+        latent_dim=128,
+        action_head_architecture=[128],
+        state_head_architecture=[128],
+        latent_mlp_architecture=[64, 32],
+        memory_optimization=False,
+        lr: float =  1e-4,
+        *args,
+        **kwargs
+    ):
+        super().__init__(
+            state_dim,
+            nvec,
+            latent_dim,
+            action_head_architecture,
+            state_head_architecture,
+            latent_mlp_architecture,
+            memory_optimization,
+            *args,
+            **kwargs
+        )
+
+        self.criterion = nn.MSELoss()
+        self.optimizer = torch.optim.AdamW(self.parameters(), lr=lr)
+
+    def train_net(
+        self, mini_batch: Tuple[Tensor, Tensor, Tensor, Tensor, Tensor], target: Tensor
+    ) -> Dict[str, float]:
+        state, action, _, _, _ = mini_batch
+
+        action = action.int()
+        if action.shape[1] == 1:
+            action = action[:, 0]
+            
+        # get only diagonal values because  of one to one relation
+        prediction = self.one2one_forward(state, action)
+        # print(prediction.shape, target.shape)
+        loss = self.criterion.forward(prediction, target)
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
+
+        return {"critic_loss": loss.item()}
