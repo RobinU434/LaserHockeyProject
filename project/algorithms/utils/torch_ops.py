4ad6eac9d5a8e9a093c1d0dea733ee2f99208384
Robin Uhrich <robin.uhrich@gmail.com>
2025-02-16 23:37:11 +0100

    bugfix in state dict to function

--- project/algorithms/utils/torch_ops.py
+++ project/algorithms/utils/torch_ops.py
@@ -3,6 +3,7 @@ from typing import Dict
 import torch
 from torch import Tensor
 
+
 def detach_tensor(*args):
     res = []
     for t in args:
@@ -14,12 +15,19 @@ def detach_tensor(*args):
 def state_dict_to(
     state_dict: Dict[str, Tensor], device: torch.device
 ) -> Dict[str, Tensor]:
-    return {k: v.to(device) for k, v in state_dict.items()}
+    res = {}
+    for k, v in state_dict.items():
+        if isinstance(v, Tensor):
+            v = v.to(device)
+        elif isinstance(v, dict):
+            v = state_dict_to(v, device)
+        res[k] = v
+    return res
 
 
 def state_dict_to_cpu(state_dict: Dict[str, Tensor]) -> Dict[str, Tensor]:
-    return {k: v.cpu() for k, v in state_dict.items()}
+    return state_dict_to(state_dict, torch.device("cpu"))
 
 
 def state_dict_to_cuda(state_dict: Dict[str, Tensor]) -> Dict[str, Tensor]:
-    return {k: v.cuda() for k, v in state_dict.items()}
+    return state_dict_to(state_dict, torch.device("cuda:0"))
