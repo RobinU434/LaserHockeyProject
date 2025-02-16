350f1eefcdbbedb5ebe012f2b2b92e3f78e97c08
Robin Uhrich <robin.uhrich@gmail.com>
2025-02-17 00:40:53 +0100

    added c2c registry

--- c2c.sh
+++ c2c.sh
@@ -3,4 +3,5 @@
 config2class stop-all
 
 config2class to-code --input config/training/train_sac.yaml --output project/utils/configs/train_sac_config.py --init-none
+config2class to-code --input config/training/train_dyna.yaml --output project/utils/configs/train_dyna_config.py --init-none
 
