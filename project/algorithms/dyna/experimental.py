ecd10aff258f40d4006ef446bd41ada1e9e85934
Robin Uhrich <robin.uhrich@gmail.com>
2025-02-18 11:39:57 +0100

    added experimental dyna with entropy regularization

---
+++ project/algorithms/dyna/experimental.py
@@ -0,0 +1,96 @@
+import numpy as np
+import torch
+from project.algorithms.dyna.dyna import DynaQ
+from torch import optim
+from torch.nn import functional as F
+
+
+class ERDyna(DynaQ):
+    """entropy regularized dyna.
+
+    don't set a decay rate for epsilon greedy policies.
+    Let the network decide how greedy it would like to be based on a target entropy
+
+    """
+
+    def __init__(
+        self,
+        env,
+        logger=...,
+        eval_env=None,
+        eval_check_interval=None,
+        save_interval=None,
+        log_dir=None,
+        batch_size=256,
+        buffer_limit=50000,
+        start_buffer_size=1000,
+        target_entropy: float = None,  # 0 = point mass
+        init_alpha: float = 1e-4,  # small emphasis randomness
+        lr_alpha: float = 1e-3,
+        gamma=0.99,
+        tau=0.01,
+        simulation_updates=2,
+        *args,
+        **kwargs
+    ):
+        super().__init__(
+            env,
+            logger,
+            eval_env,
+            eval_check_interval,
+            save_interval,
+            log_dir,
+            batch_size,
+            buffer_limit,
+            start_buffer_size,
+            0,  # not needed
+            gamma,
+            tau,
+            simulation_updates,
+            *args,
+            **kwargs
+        )
+
+        self.target_entropy = target_entropy
+        if self.target_entropy is None:
+            self.target_entropy = np.log(self._n_actions)
+        self.init_alpha = init_alpha
+
+        self.log_alpha = torch.tensor(np.log(init_alpha))
+        self.log_alpha.requires_grad = True
+        self.log_alpha_optimizer = optim.AdamW([self.log_alpha], lr=lr_alpha)
+
+    def get_action(self, state, episode_idx):
+        q_val = self.q_net.complete_forward(state)
+        action_log_probs = F.log_softmax(torch.exp(self.log_alpha) * q_val)
+
+        action = np.random.choice(
+            len(action_log_probs), p=torch.exp(action_log_probs).detach().numpy()
+        )
+        log_prob = action_log_probs[action]
+
+        return action, log_prob
+
+    def update_q(self, mini_batch, episode_idx):
+        super().update_q(mini_batch, episode_idx)
+
+        # optimize alpha
+        alpha = torch.exp(self.log_alpha)
+        (
+            state,
+            _,
+            _,
+            _,
+            _,
+        ) = mini_batch
+        q_val = self.q_net.complete_forward(state)
+        action_prob = F.softmax(alpha * q_val, dim=-1)
+        action_prob = action_prob + 1e-18  # avoid 0
+        entropy = -torch.sum(action_prob * torch.log(action_prob))
+
+        loss = -alpha * (entropy - self.target_entropy)
+        self.log_alpha_optimizer.zero_grad()
+        loss.backward()
+        self.log_alpha_optimizer.step()
+
+        self.log_scalar("Dyna/alpha_loss", loss.item(), episode_idx)
