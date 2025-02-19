869377e59a9b572abbc66544fd02f36184e58090
Robin Uhrich <robin.uhrich@gmail.com>
2025-02-19 15:01:13 +0100

    added new observer to c2c

--- c2c.sh
+++ c2c.sh
@@ -4,4 +4,4 @@ config2class stop-all
 
 config2class to-code --input config/training/train_sac.yaml --output project/utils/configs/train_sac_config.py --init-none
 config2class to-code --input config/training/train_dyna.yaml --output project/utils/configs/train_dyna_config.py --init-none
-
+config2class to-code --input config/training/train_md_dyna.yaml --output project/utils/configs/train_md_dyna_config.py --init-none
