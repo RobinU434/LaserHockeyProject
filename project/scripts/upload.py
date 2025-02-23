774b767028fdfb103cd1fa9f77acc09f507723e2
Robin Uhrich <robin.uhrich@gmail.com>
2025-02-23 12:01:02 +0100

    bugifx in upload

--- project/scripts/upload.py
+++ project/scripts/upload.py
@@ -2,5 +2,5 @@ from project.algorithms.sb3_extensions.sac import SB_SAC_Agent
 
 
 def upload_sb_sac_agent(checkpoint, server_url, server_port, token):
-    agent = SB_SAC_Agent(checkpoint)
+    agent = SB_SAC_Agent.from_checkpoint(checkpoint)
     agent.run(token, server_url, server_port)
